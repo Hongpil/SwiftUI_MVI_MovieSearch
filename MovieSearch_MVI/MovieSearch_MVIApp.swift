a72edb9e46bce602dffac9c2cb4de946a7e7d63c
kimhongpil <idpil@kimhongpilui-MacBookPro.local>
2023-01-29 06:33:33 +0900

    Set Code

---
+++ MovieSearch_MVI/MovieSearch_MVIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearch_MVIApp.swift
+//  MovieSearch_MVI
+//
+//  Created by kimhongpil on 2023/01/29.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearch_MVIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
